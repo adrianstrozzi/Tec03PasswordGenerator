0ae909e51abca99dc882eb9d32fbd68369abb8f7
Adrian Strozzi <adrianstrozzi@gmail.com>
2022-03-22 22:55:31 -0600

    Added function to read password length input.

--- Develop/script.js
+++ Develop/script.js
@@ -1,38 +1,31 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var passwordLetters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var passwordNums = "0123456789"
-var passwordSpecialChars = "!@#$%^&*()"
 
-// var passwordChars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var passwordLength = document.getElementById("length")
+var includeUpperCaseLetters = document.getElementById("upperCaseLetters")
+var includeLowerCaseLetters = document.getElementById("LowerCaseLetters")
+var includeNumbers = document.getElementById("numbers")
+var includeSpecial = document.getElementById("special")
 
-function generatePassword() {
-  generateBtn.addEventListener('click', function () {
-    var passwordLength = prompt("Please choose a password length between 8-128 characters.")
-    if (
-      isNaN(passwordLength) ||
-      passwordLength < 8 || passwordLength > 128
-    ) {
-      prompt("Please choose password length number between 8 & 128.");
-    }
-    else {
-      alert("Password generated with " + passwordLength + " characters.")
-    }
-    var passwordIncludeLetters = prompt("Do you want your password to include letters? Choose Y or N.")
-    if (passwordIncludeLetters === "Y"
-    ) {
-      alert("Your password will include letters");
-    }
-    else if (passwordIncludeLetters === "N") {
-      alert("Your password will not include letters");
-    }
-    else {
-      prompt("Please choose Y or N");
-    }
-  })
+// var passwordLetters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+// var passwordNumbers = "0123456789"
+// var passwordSpecial = "!@#$%^&*()"
+
+
+passwordLength.addEventListener('input', passwordLengthInput)
+
+
+// Read input of password length
+function passwordLengthInput(event) {
+  var value = event.target.value
+  passwordLength.value = value
 }
 
+// Function for generating the password with selected parameters
 
+// function generatePassword(passwordLength, includeUpperCaseLetters, includeLowerCaseLetters, includeNumbers, includeSpecial) {
+
+// }
 
 
 
@@ -44,5 +37,38 @@ function writePassword() {
   passwordText.value = password;
 }
 
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+
+// function generatePassword() {
+//   generateBtn.addEventListener('click', function () {
+//     var passwordLength = prompt("Please choose a password length between 8-128 characters.")
+//     if (
+//       isNaN(passwordLength) ||
+//       passwordLength < 8 || passwordLength > 128
+//     ) {
+//       prompt("Please choose password length number between 8 & 128.");
+//     }
+//     else {
+//       alert("Password generated with " + passwordLength + " characters.")
+//     }
+//     var passwordIncludeLetters = prompt("Do you want your password to include letters? Choose Y or N.")
+//     if (passwordIncludeLetters === "Y"
+//     ) {
+//       alert("Your password will include letters");
+//     }
+//     else if (passwordIncludeLetters === "N") {
+//       alert("Your password will not include letters");
+//     }
+//     else {
+//       prompt("Please choose Y or N");
+//     }
+//   })
+// }
